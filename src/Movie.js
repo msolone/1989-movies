12c9e82fdf59ff7a9502c2d4c02f4e1989217561
Michael Solone <michaelsolone@gmail.com>
2018-08-13 21:23:31 -0400

    Sorts movie by release date

--- src/Movie.js
+++ src/Movie.js
@@ -5,6 +5,7 @@ class Movie extends Component {
     return (
       <li className="movie">
         <h2>{this.props.title}</h2>
+        <h3>{this.props.release_date}</h3>
         <img src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2${this.props.poster_path}`} alt="image" />
         <p>{this.props.overview}</p>
       </li>
--- src/MovieList.js
+++ src/MovieList.js
@@ -3,13 +3,31 @@ import Movie from './Movie.js'
 import MovieData from './1989-Movies.json'
 
 class MovieList extends Component {
-  render() {
+    // sortByNumber = (a,b) => {
+    //     return a - b
+    // }
+    render() {
+      let splitDate = MovieData.results[0].release_date.split('-').join('')
+      let newReleaseDate = parseInt(splitDate)
+      console.log(newReleaseDate)
+      
     return (
-      <div className="movieList">
-        <ul>
-
-            {MovieData.results.map((movie, i) =>
-            {return <Movie title={movie.title} poster_path={movie.poster_path} overview={movie.overview} key={i} />
+      <div>
+        <ul className="movieListUl">
+            {MovieData.results.sort((a,b) => {
+                if (a.release_date > b.release_date) {
+                    return 1
+                } else if (a.release_date < b.release_date) {
+                    return -1
+                } else {
+                    return 0
+                }
+            }).map((movie, i) =>
+            {return <Movie title={movie.title} 
+            release_date={movie.release_date} 
+            poster_path={movie.poster_path} 
+            overview={movie.overview} 
+            key={i} />
                 })}
         </ul>
 
